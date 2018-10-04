033db9cc99f87876573acf7ae18ed26f2c86a561
Robin Tram <robin.tram@gmail.com>
2018-10-04 16:20:30 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,7 @@ def nyc_pigeon_organizer(data)
   
   pigeon_data.each do |attribute, attribute_value|
     attribute_value.each do |attribute_value, bird_array|
-      
+      bird_array.each do |bird_name|
+        
     
 end
