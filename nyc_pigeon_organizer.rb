0ce8215f07400b9df95ab704a92b5f0a1f6eb27b
Robin Tram <robin.tram@gmail.com>
2018-10-04 16:36:32 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,10 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, attribute_value|
     attribute_value.each do |attribute_value, bird_array|
       bird_array.each do |bird_name|
-        binding.pry
+                binding.pry
+        if (pigeon_list.keys.include? bird_name) == false 
+        pigeon_list[bird_name] = bird_name => {:color => [], :gender => [], :lives []}
+
 
       end 
     end 
